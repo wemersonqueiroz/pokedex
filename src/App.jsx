ecc3f7ed15f069777eea528ab6922a1ee99edddb
Wemerson <wemersonqueiroz@yahoo.com>
2022-08-26 15:14:16 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from "react"
-
 import pokemonLogo from "./assets/pokemon.png"
-import Pokedex from "./components/Pokedex"
-
-import { ButtonPrevious, ButtonNext } from "./components/Button"
 import Search from "./components/Search"
 
 import { searchPokemon } from "./api"
@@ -11,17 +7,10 @@ import { searchPokemon } from "./api"
 import "./index.css"
 
 function App() {
-
   return (
     <>
       <div className="App">
-        <img className="pokemonLogo" src={pokemonLogo} alt="pokemonLogo" />
-        <div className="pokedexContainer">
-          <Pokedex />
-
-          <Search/>
-          <div className="pokemonList"></div>
-        </div>
+        <Search />
       </div>
     </>
   )
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,29 @@
 import React, { useState, useEffect } from "react"
 import { searchPokemon } from "../api"
-
+import pokedex from "../assets/pokedex.png"
+import pokemonLogo from "../assets/pokemon.png"
 const Search = () => {
-  const [search, setSearch] = useState("charizard")
+  const [search, setSearch] = useState("ditto")
   const [pokemon, setPokemon] = useState()
 
   const onChangeHandler = event => {
     setSearch(event.target.value)
   }
 
+  const pressEnter = event => {
+    if (event.key === "Enter") {
+      searchPokemon(search).then(data => {
+        setPokemon(data)
+      })
+    }
+  }
 
+  useEffect(() => {
+    document.addEventListener("keypress", pressEnter)
+    return () => {
+      document.removeEventListener("keypress", pressEnter)
+    }
+  }, [search])
 
   const onButtonClickHandler = () => {
     onSearchHandler(search)
@@ -18,45 +32,43 @@ const Search = () => {
   const onSearchHandler = async pokemon => {
     const result = await searchPokemon(pokemon)
     setPokemon(result)
-
   }
 
-
-
   return (
-    <div className="search">
-      <input
-        className="searchBar"
-        type="text"
-        placeholder="Search"
-        onChange={onChangeHandler}
-      />
-      <button className="searchButton" onClick={onButtonClickHandler}>
-        <i className="fas fa-search " />
-      </button>
-      {pokemon ? (
-        <div>
-          <p className="pokemonName">{pokemon.name}</p>
-          <p className="pokemonId">#{pokemon.id}</p>
-          <p className="pokemonHeight">Height: {pokemon.height} M</p>
-          <p className="pokemonWeight">Weight: {pokemon.weight} Kgs</p>
-          <p className="pokemonTypes">
-            Type: {pokemon.types.map(type => type.type.name).join(", ")}
-          </p>
-          <img
-            className="pokemonImage"
-            src={pokemon.sprites.front_default}
-            alt="pokemon"
-          />
-        </div>
-      ) : (
-        <p>Pokemon Not Found!</p>
-      )}
+    <div className="search ">
+      <img className="pokemonLogo" src={pokemonLogo} alt="pokemonLogo" />
+
+      <div className="containerPokedex">
+        <img className="pokedexImage" src={pokedex} alt="pokedex" />
+        <input
+          className="searchBar"
+          type="text"
+          placeholder="Search"
+          onKeyDown={pressEnter}
+          onChange={onChangeHandler}
+        />
+        <button className="searchButton" onClick={onButtonClickHandler}>
+          <i className="fas fa-search " />
+        </button>
+        {pokemon ? (
+          <>
+            <div className="pokemonName">{pokemon.name}</div>
+            <div className="pokemonId">#{pokemon.id}</div>
+            <div className="pokemonHeight">Height: {pokemon.height} M</div>
+            <div className="pokemonWeight">Weight: {pokemon.weight} Kgs</div>
+            <div className="pokemonTypes">
+              Type: {pokemon.types.map(type => type.type.name).join(", ")}
+            </div>
+            <div className="pokemonImage">
+              <img src={pokemon.sprites.front_default} alt="pokemon" />
+            </div>
+          </>
+        ) : (
+          <div className="pokemonNotFound"> Pokemon Not Found!</div>
+        )}
+      </div>
     </div>
   )
 }
 
-
-
-
-export default Search;
+export default Search
